9b2bbbad739d421d30112534edd98133f271d847
Ivan da Silva Rosa <iivan.si@msn.com>
2022-01-10 23:16:46 -0300

    project starter

---
+++ APIRestAlgaLogistics/src/main/java/com/algaworks/APIRestAlgaLogistics/ApiRestAlgaLogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.APIRestAlgaLogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestAlgaLogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestAlgaLogisticsApplication.class, args);
+	}
+
+}
---
+++ APIRestAlgaLogistics/src/test/java/com/algaworks/APIRestAlgaLogistics/ApiRestAlgaLogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.APIRestAlgaLogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestAlgaLogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
